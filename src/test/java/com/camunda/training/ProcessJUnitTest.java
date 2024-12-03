2b90c165f5a9a8d93942d934200ee7a5a8456919
maksim.lastenko <maksim.lastenko@axenix.pro>
2024-12-03 19:47:25 +0500

    add first test

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,10 +1,30 @@
 package com.camunda.training;
 
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
+
+@ExtendWith(ProcessEngineExtension.class)
 public class ProcessJUnitTest {
-  
-//  @Test
-  public void testHappyPath() {
-    
-  }
+
+    @Test
+    @Deployment(resources = "Twitter_QA.bpmn")
+    public void testHappyPath() {
+        // Create a HashMap to put in variables for the process instance
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("approved", true);
+        // Start process with Java API and variables
+        ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
+        // Make assertions on the process instance
+        assertThat(processInstance).isEnded();
+    }
 
 }
